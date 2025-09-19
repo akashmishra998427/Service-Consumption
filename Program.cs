c999171ca617ad9ce2c402e91b50682a1360fa87
Akash Mishra <akashmishra998427@gmail.com>
2025-09-19 09:32:01 -0700

    cleanup code

--- Program.cs
+++ Program.cs
@@ -1,37 +1,3 @@
-//using FluentValidation;
-//using FluentValidation.AspNetCore;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-
-//// Register FluentValidation
-//builder.Services.AddControllers();
-//builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-//// This scans the assembly and registers all validators
-
-//builder.Services.AddFluentValidationAutoValidation(); // Enables auto-validation
-//builder.Services.AddFluentValidationClientsideAdapters();
-
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-//app.Run();
-
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using ServiceConsumption.Models.Validation;
@@ -40,21 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-//// Register validators in DI
-//builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-
-//// Enable FluentValidation automatic model validation
-//builder.Services.AddFluentValidationAutoValidation();
-//builder.Services.AddFluentValidationClientsideAdapters();
-
 builder.Services.AddControllers()
     .AddFluentValidation(fv =>
     {
-        // Register validators automatically from your assembly
         fv.RegisterValidatorsFromAssemblyContaining<PostApiEntityValidations>();
     });
-
-//builder.Services.AddScoped<PostApiEntityValidations>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
